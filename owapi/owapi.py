ead65102d3b8100e13c2e151596cf2f5b0a597ee
kokomi simp :3 <beol0127@gmail.com>
2022-03-15 12:04:36 +0000

    chore: clean up code

--- owapi/owapi.py
+++ owapi/owapi.py
@@ -3,19 +3,27 @@ import os
 import requests
 import json
 
+# error's
 class noPermission(Exception):
   pass
 
+url = "";
+
+# client
 class Client:
   def __init__(self, key):
     self.key = key
+
+
+  
 
 class Assets:
   def __init__(self, password=None):
     self.password = password
 
-    if self.password == None:
-      raise noPermission(f"'{self.password}' must be filled")
+    if self.password == None or self.password != os.getenv('password'):
+      raise noPermission(f"'{self.password}' must be filled or is incorrect")
 
   def parse_json(tjson):
     return json.dumps(tjson, sort_keys=False, indent=2)
+
