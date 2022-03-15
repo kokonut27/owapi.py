9fa7422a84c1ae29225c142b4a46b97fbfca001a
kokomi simp :3 <beol0127@gmail.com>
2022-03-15 12:00:24 +0000

    chore: update config + add parsing json

--- owapi/owapi.py
+++ owapi/owapi.py
@@ -1,8 +1,21 @@
+#!/usr/bin/python
 import os
 import requests
 import json
+
+class noPermission(Exception):
+  pass
 
 class Client:
   def __init__(self, key):
     self.key = key
 
+class Assets:
+  def __init__(self, password=None):
+    self.password = password
+
+    if self.password == None:
+      raise noPermission(f"'{self.password}' must be filled")
+
+  def parse_json(tjson):
+    return json.dumps(tjson, sort_keys=False, indent=2)
